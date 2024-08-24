0357980efd9ef03709dbe65d1de51042f06652bd
a.adishesh.gaekwad <a.adishesh.gaekwad@accenture.com>
2024-08-24 12:47:28 +0530

    first version - basic structure with all components and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { OverviewComponent } from './overview/overview.component';
+import { SearchEmployeeComponent } from './search-employee/search-employee.component';
+import { ManageEmployeeComponent } from './manage-employee/manage-employee.component';
+import { MyAccountComponent } from './my-account/my-account.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: OverviewComponent },
+  { path: "search-employee", component: SearchEmployeeComponent },
+  { path: "manage-employee", component: ManageEmployeeComponent },
+  { path: "my-account", component: MyAccountComponent },
+];
+
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SideNavComponent } from './side-nav/side-nav.component';
+import { MainSectionComponent } from './main-section/main-section.component';
+import { OverviewComponent } from './overview/overview.component';
+import { SearchEmployeeComponent } from './search-employee/search-employee.component';
+import { ManageEmployeeComponent } from './manage-employee/manage-employee.component';
+import { MyAccountComponent } from './my-account/my-account.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SideNavComponent,
+    MainSectionComponent,
+    OverviewComponent,
+    SearchEmployeeComponent,
+    ManageEmployeeComponent,
+    MyAccountComponent
   ],
   imports: [
     BrowserModule,
